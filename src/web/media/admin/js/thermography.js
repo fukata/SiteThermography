d7b83057925edd95d37fa96fa26e3889875dc416
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-05-09 14:58:59 +0800

    scale変更

--- src/web/media/admin/js/thermography.js
+++ src/web/media/admin/js/thermography.js
@@ -3,8 +3,7 @@
 	$frame.height($(window).height()-20);
 	var $fw = $($frame[0].contentWindow);
 	$frame.bind('load.thermography', function(){
-		console.log("thermography_frame loaded.");
 		var $fb = $($frame[0].contentWindow.document.body);
-		$fb.heatmap({scale:30}, points, max_point);
+		$fb.heatmap({}, points, max_point);
 	});
 })(jQuery);
